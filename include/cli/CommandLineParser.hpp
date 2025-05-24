300ec505bc3cd4263ce4a6f92e04eaa5e4b67d6b
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-24 15:24:48 -0700

    Added default parameter and a set of acceptable models

--- include/cli/CommandLineParser.hpp
+++ include/cli/CommandLineParser.hpp
@@ -4,10 +4,14 @@
 #include <vector>
 #include <string_view>
 #include <stdexcept>
+#include <unordered_set>
+
+static constexpr std::string DEFAULT_MODEL_NAME{"Ollama3.2"};
 
 class CommandLineParser {
 private:
-  std::string modelName_;
+  std::string modelName_ = DEFAULT_MODEL_NAME;
+  static const std::unordered_set<std::string_view> acceptedModels_;
 
 public:
   void parseModelName(int argc, const char* argv[]);
