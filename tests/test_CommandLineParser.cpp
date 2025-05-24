089e2e999853460fd30c593b0d4d6fcaa8cd87c4
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-24 01:55:11 -0700

    Added parse_model_name_success_test

---
+++ tests/test_CommandLineParser.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_MODULE  CommandLineParserTest
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "cli/CommandLineParser.hpp"
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(parse_model_name_success) {
+  CommandLineParser parser;
+
+  const char* argv[] = {"program", "--model", "llama3.2"};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+
+  parser.parseModelName(argc, argv);
+
+  BOOST_CHECK_EQUAL(parser.getModelName(), "llama3.2");
+}
+
