507cd76999d1bfdf0ff2aab038fc9b068504acab
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-25 00:32:14 -0700

    Improved the structure for consistency

--- include/cli/CommandLineParser.hpp
+++ include/cli/CommandLineParser.hpp
@@ -5,17 +5,29 @@
 #include <string_view>
 #include <stdexcept>
 #include <unordered_set>
+#include <iostream>
 
 static constexpr std::string DEFAULT_MODEL_NAME{"Ollama3.2"};
 
 class CommandLineParser {
+public:
+  CommandLineParser(OllamaAPIClient ollama,
+                    PostgresDatabaseManager db,
+                    JobApplication job)
+    :  ollamaLLM_(ollama), databaseManager_(db), jobApplication_(job) {}
+
+  void parseModelName(int argc, const char* argv[]);
+
+  std::string getModelName() const { return modelName_; }
+
+  void submitJobApplication();
+
 private:
   std::string modelName_ = DEFAULT_MODEL_NAME;
   static const std::unordered_set<std::string_view> acceptedModels_;
-
-public:
-  void parseModelName(int argc, const char* argv[]);
-  std::string getModelName() const { return modelName_; }
+  OllamaAPIClient ollamaLLM_;
+  PostgresDatabaseManager databaseManager_;
+  JobApplication& jobApplication_;
 };
 
 #endif // COMMANDLINEPARSER_HPP
