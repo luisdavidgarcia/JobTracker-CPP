3f6f360b10a2293926dcb469b748c3aca8b620c7
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-25 12:20:21 -0700

    Simpified core class to follow proper RAII

--- include/app/JobTracker.hpp
+++ include/app/JobTracker.hpp
@@ -2,8 +2,8 @@
 #define JOBTRACKER_HPP
 
 #include <iostream>
+
 #include "cli/CommandLineParser.hpp"
-#include "data/JobApplication.hpp"
 #include "db/PostgresDatabaseManager.hpp"
 #include "llm/OllamaAPIClient.hpp"
 
@@ -11,20 +11,12 @@ class JobTracker {
 public:
   JobTracker(int argc, char* argv[]);
 
-  static JobTracker create(int argc, char* argv[]);
-  
   void run();
 
 private:
-  JobTracker(CommandLineParser parser,
-             OllamaAPIClient ollama,
-             PostgresDatabaseManager db,
-             JobApplication job);
-
   CommandLineParser commandParser_;
   OllamaAPIClient ollamaLLM_;
   PostgresDatabaseManager databaseManager_;
-  JobApplication jobApplication_;
 };
 
 #endif // JOBTRACKER_HPP
