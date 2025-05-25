712b32f496136ee8323a83005e9592787dddec78
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-25 00:14:06 -0700

    Made JobApplication strucutre for store job apps

---
+++ include/data/JobApplication.hpp
@@ -0,0 +1,18 @@
+#ifndef JOBAPPLICATION_HPP
+#define JOBAPPLICATION_HPP
+
+#include <string>
+#include <vector>
+#include <chrono>
+
+struct JobApplication {
+  int id = -1;
+  std::string company_name;
+  std::string position_title;
+  std::chrono::year_month_day applied_date;
+  std::string salary_range;
+  std::string company_challenge;
+  std::vector<std::string> required_skills;
+};
+
+#endif // JOBAPPLICATION_HPP
