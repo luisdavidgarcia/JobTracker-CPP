1e8e450532714d64bcc3b0312a6a520cb9027ce0
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-25 12:17:26 -0700

    Simplifed structure of database manager to reduce circular dependencies

--- include/db/PostgresDatabaseManager.hpp
+++ include/db/PostgresDatabaseManager.hpp
@@ -1,18 +1,18 @@
 #ifndef JOBAPPLICATION_HPP
 #define JOBAPPLICATION_HPP
 
-static constexpr int DEFAULT_PORT_NUMBER = 5432;
+#include "data/JobApplication.hpp"
+
+struct JobApplication;
 
 class PostgresDatabaseManager {
 public:
-  PostgresDatabaseManager(JobApplication& jobApp)
-    : jobApplication_(jobApp) {}
-
-  void sumbitQuery();
+  void insertJobApplication(const JobApplication& job);
 
 private:
+  static constexpr int DEFAULT_PORT_NUMBER = 5432;
+
   int portNumber_ = DEFAULT_PORT_NUMBER;
-  JobApplication& jobApplication_;
 
   void craftQuery();
 };
