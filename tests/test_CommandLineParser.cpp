c97a67bf96763e5db2dea0730c67b7f4a571a342
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-25 12:19:10 -0700

    Made test file into CommandLine Suite

--- tests/test_CommandLineParser.cpp
+++ tests/test_CommandLineParser.cpp
@@ -1,9 +1,10 @@
-#define BOOST_TEST_MODULE  CommandLineParserTest
+#define BOOST_TEST_MODULE JobTrackerTests
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-
 #include "cli/CommandLineParser.hpp"
 #include <iostream>
+
+BOOST_AUTO_TEST_SUITE(CommandLineParserTests)
 
 BOOST_AUTO_TEST_CASE(parse_model_name_success) {
   CommandLineParser parser;
@@ -25,3 +26,15 @@ BOOST_AUTO_TEST_CASE(parse_model_missing_model_args) {
   
   BOOST_CHECK_THROW(parser.parseModelName(argc, argv), std::runtime_error);
 }
+
+BOOST_AUTO_TEST_CASE(parse_incomptabile_model) {
+  CommandLineParser parser;
+  
+  const char* argv [] = {"program", "--model", "xyz"};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+
+  BOOST_CHECK_THROW(parser.parseModelName(argc, argv), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
