9c96ec107dd6a9066c724383ea6ef0bcdeae9867
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-25 12:16:52 -0700

    Decoupled logic of CommandLineParser so that it  serves as stand alone library

--- include/cli/CommandLineParser.hpp
+++ include/cli/CommandLineParser.hpp
@@ -7,27 +7,19 @@
 #include <unordered_set>
 #include <iostream>
 
-static constexpr std::string DEFAULT_MODEL_NAME{"Ollama3.2"};
+static constexpr std::string DEFAULT_MODEL_NAME{"llama3.2"};
 
 class CommandLineParser {
 public:
-  CommandLineParser(OllamaAPIClient ollama,
-                    PostgresDatabaseManager db,
-                    JobApplication job)
-    :  ollamaLLM_(ollama), databaseManager_(db), jobApplication_(job) {}
-
   void parseModelName(int argc, const char* argv[]);
 
-  std::string getModelName() const { return modelName_; }
+  std::string promptJobDescriptionORExit();
 
-  void submitJobApplication();
+  std::string getModelName() const { return modelName_; }
 
 private:
   std::string modelName_ = DEFAULT_MODEL_NAME;
   static const std::unordered_set<std::string_view> acceptedModels_;
-  OllamaAPIClient ollamaLLM_;
-  PostgresDatabaseManager databaseManager_;
-  JobApplication& jobApplication_;
 };
 
 #endif // COMMANDLINEPARSER_HPP
