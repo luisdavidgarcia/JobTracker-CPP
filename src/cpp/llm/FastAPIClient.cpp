8ab93f40f0eb17a259f54a9a8139be19976c7b34
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-28 00:30:48 -0700

    Added sendJobDescription

--- src/cpp/llm/FastAPIClient.cpp
+++ src/cpp/llm/FastAPIClient.cpp
@@ -17,3 +17,23 @@ auto FastAPIClient::close() -> void {
     throw boost::beast::system_error{ec};
 }
 
+auto FastAPIClient::sendJobDescription(const std::string& jobDescription) -> void {
+  boost::beast::http::request<boost::beast::http::string_body> req{
+    boost::beast::http::verb::post, 
+    target_, 
+    version_
+  };
+  req.set(boost::beast::http::field::host, host_);
+  req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+  req.body() = jobDescription;
+  req.prepare_payload();
+
+  boost::beast::http::write(stream_, req);
+
+  boost::beast::flat_buffer buffer;
+  boost::beast::http::response<boost::beast::http::dynamic_body> res;
+  boost::beast::http::read(stream_, buffer, res);
+
+  std::cout << "Parsed Job Description from FastAPI: " << std::endl;
+  std::cout << res << std::endl;
+}
